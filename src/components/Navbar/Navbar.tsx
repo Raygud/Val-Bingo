e79b9f12259469ec3d775bd1d961b2f19725961c
Raygud <runigudmundarson@hotmail.com>
2022-12-05 22:10:02 +0100

    Finished withing 24 hours!

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import { Link, Outlet } from "react-router-dom";
+import { BsGithub, BsLinkedin } from 'react-icons/bs';
 import './Navbar.scss'
 
-const Navbar =() => {
-    return (
-        <div>
-          
-          <nav>
-                <ol>
-                    <li><a href="https://github.com/" rel="noreferrer" target={"_blank"}>Github</a></li>
-                    <li><Link to="/leikreglur">Leikreglur</Link></li>
-                    <li><a href="https://www.linkedin.com/" rel="noreferrer" target={"_blank"}>Linkedin</a></li>
-                </ol>
-          </nav>
-        
-          <Outlet />
-        </div>
-      );
-  }
+const Navbar = () => {
 
-  export default Navbar;
+  const iconStyle = { color: 'black', fontSize: '10vw', margin: '1vw' }
+  return (
+    <div>
+
+      <nav>
+        <ol>
+          <li><a href="https://github.com/Raygud" rel="noreferrer" target={"_blank"}><BsGithub style={iconStyle} /></a></li>
+          <li><Link to="/leikreglur">Leikreglur</Link></li>
+          <li><a href="https://www.linkedin.com/in/r%C3%BAni-gudmundarson-b33559176/" rel="noreferrer" target={"_blank"}><BsLinkedin style={iconStyle} /></a></li>
+        </ol>
+      </nav>
+
+      <Outlet />
+    </div>
+  );
+}
+
+export default Navbar;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,74 +1,84 @@
 import './Home.scss'
-import {useState, useEffect} from 'react'
-import { BsListUl,BsGrid } from 'react-icons/bs';
+import { useState, useEffect } from 'react'
+import { BsListUl, BsGrid, BsGithub, BsLinkedin } from 'react-icons/bs';
 import { parse } from 'node:path/win32';
 
 
-export const Home =() => {
-    const unSortedNumbers:any[] = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
-    for (let i = unSortedNumbers.length -1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i+1));
+export const Home = () => {
+    const unSortedNumbers: any[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+    const consequences: any[] = ["Drekk 2 smakkir1", "Drekk 2 smakkir1", "Drekk 2 smakkir2", "Drekk 2 smakkir3", "Drekk 2 smakkir4", "Drekk 2 smakkir5", "Drekk 2 smakkir6", "Drekk 2 smakkir7", "Drekk 2 smakkir8", "Drekk 2 smakkir9", "Drekk 2 smakkir10", "Drekk 2 smakkir11", "Drekk 2 smakkir12", "Drekk 2 smakkir13", "Drekk 2 smakkir14", "Drekk 2 smakkir15", "Drekk 2 smakkir16"]
+    for (let i = unSortedNumbers.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
         let k = unSortedNumbers[i];
         unSortedNumbers[i] = unSortedNumbers[j];
         unSortedNumbers[j] = k;
-      }
+    }
 
     const [plateNumbers, setPlateNumbers] = useState<any[]>([...Array(16)])
     const [isClicked, setIsClicked] = useState<any[]>([])
+    const [oldValue, setOldValue] = useState<boolean>(false)
+    const [lastClicked, setLastClicked] = useState<boolean>(false)
     const [layout, setLayout] = useState<string>("grid")
 
     const [isSet, setSet] = useState<any>(true)
-    const iconStyle = {color:'black', fontSize:'8vw', margin:'1vw'}
+    const iconStyle = { color: 'black', fontSize: '8vw', margin: '1vw' }
 
-    const onClick = (Clicked:any) =>{
+    const onClick = (Clicked: any) => {
         console.log(isClicked)
-        if(!isClicked.includes(Clicked)){
-        setIsClicked(oldArray => [...oldArray, Clicked]);
+        if (!isClicked.includes(Clicked)) {
+            setIsClicked(oldArray => [...oldArray, Clicked]);
         }
-        else{
+        else {
             setIsClicked(isClicked.filter(item => item !== Clicked));
         }
     }
 
     useEffect(() => {
-        if(localStorage.getItem('ClickedNumbers')&& isSet){
-            setSet(false)
+        if (localStorage.getItem("ClickedNumbers") && oldValue === false) {
             setIsClicked(JSON.parse(localStorage.getItem('ClickedNumbers') as any))
+            setOldValue(true)
+
+        } else {
+            setOldValue(true)
+
         }
-        if(!isSet){
-        localStorage.setItem('ClickedNumbers', JSON.stringify(isClicked));
+        if (oldValue === true) {
+            localStorage.setItem('ClickedNumbers', JSON.stringify(isClicked));
         }
+
+        setLastClicked(consequences[isClicked[isClicked.length - 1]])
     }, [isClicked])
 
     useEffect(() => {
-        if(!localStorage.getItem("StoredNumbers")){
-
+        if (!localStorage.getItem("StoredNumbers")) {
             localStorage.setItem('StoredNumbers', JSON.stringify(unSortedNumbers));
             setPlateNumbers(unSortedNumbers)
-        }
-        else{
+        } else {
 
             setPlateNumbers(JSON.parse(localStorage.getItem('StoredNumbers') as any))
 
         }
 
-    }, [])
+    }, [isClicked])
 
-    const ToggleLayOut = (Layout:string) => {
+    const ToggleLayOut = (Layout: string) => {
         console.log(Layout)
         setLayout(Layout)
     }
-    
+
     return (
-      <div>
-        <div className="IconContainer"> <div className='LayoutButton' onClick={() => ToggleLayOut("grid")}> <BsGrid style={iconStyle} />  </div> <div className='LayoutButton' onClick={() => ToggleLayOut("list")}><BsListUl style={iconStyle} /></div></div>
-        <div className= {layout === "grid" ?'EnteriesGrid':'EnteriesList'}>
-        {plateNumbers.map((item:any, index:number) => (
-        <div key={index} className={isClicked.includes(item) ? "EnteriesItem Clicked":"EnteriesItem"} onClick={() => onClick(item)}>
-            <h6>{item}</h6>
+        <div>
+            <div className="IconContainer"> <div className='LayoutButton' onClick={() => ToggleLayOut("grid")}> <BsGrid style={iconStyle} />  </div> <div className='LayoutButton' onClick={() => ToggleLayOut("list")}><BsListUl style={iconStyle} /></div></div>
+            <div className={layout === "grid" ? 'EnteriesGrid' : 'EnteriesList'}>
+                {plateNumbers.map((item: any, index: number) => (
+                    <div key={index} className={isClicked.includes(item) ? "EnteriesItem Clicked" : "EnteriesItem"} onClick={() => onClick(item)}>
+                        <h6>{item}</h6>
+                    </div>
+                ))}
+
+            </div>
+            <h2>Straffur:</h2>
+            <h2>{lastClicked}</h2>
         </div>
-      ))}
-      </div>
-      </div>
     );
-  }
+}
